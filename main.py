c8e0e9ed7521f2eb3d6e5c6387331f405fcf642a
abvgdl <senior.abdulmanoff@yandex.ru>
2024-01-21 23:47:43 +0500

    Creating file

--- main.py
+++ main.py
@@ -2,9 +2,121 @@ import pygame
 import random
 import sys
 import os
+import time
 
 fps = 25
 width, height = 640, 480
+
+TEMPLATE_O = [['.....',
+               '.....',
+               '.OO..',
+               '.OO..',
+               '.....']]
+
+TEMPLATE_I = [['..O..',
+               '..O..',
+               '..O..',
+               '..O..',
+               '.....'],
+              ['.....',
+               '.....',
+               'OOOO.',
+               '.....',
+               '.....']]
+
+TEMPLATE_S = [['.....',
+               '.....',
+               '..OO.',
+               '.OO..',
+               '.....'],
+              ['.....',
+               '..O..',
+               '..OO.',
+               '...O.',
+               '.....']]
+
+TEMPLATE_Z = [['.....',
+               '.....',
+               '.OO..',
+               '..OO.',
+               '.....'],
+              ['.....',
+               '..O..',
+               '.OO..',
+               '.O...',
+               '.....']]
+
+TEMPLATE_L = [['.....',
+               '...O.',
+               '.OOO.',
+               '.....',
+               '.....'],
+              ['.....',
+               '..O..',
+               '..O..',
+               '..OO.',
+               '.....'],
+              ['.....',
+               '.....',
+               '.OOO.',
+               '.O...',
+               '.....'],
+              ['.....',
+               '.OO..',
+               '..O..',
+               '..O..',
+               '.....']]
+
+TEMPLATE_J = [['.....',
+               '.O...',
+               '.OOO.',
+               '.....',
+               '.....'],
+              ['.....',
+               '..OO.',
+               '..O..',
+               '..O..',
+               '.....'],
+              ['.....',
+               '.....',
+               '.OOO.',
+               '...O.',
+               '.....'],
+              ['.....',
+               '..O..',
+               '..O..',
+               '.OO..',
+               '.....']]
+
+TEMPLATE_T = [['.....',
+               '..O..',
+               '.OOO.',
+               '.....',
+               '.....'],
+              ['.....',
+               '..O..',
+               '..OO.',
+               '..O..',
+               '.....'],
+              ['.....',
+               '.....',
+               '.OOO.',
+               '..O..',
+               '.....'],
+              ['.....',
+               '..O..',
+               '.OO..',
+               '..O..',
+               '.....']]
+
+TEMPLATES = {
+    'O': TEMPLATE_O,
+    'I': TEMPLATE_I,
+    'S': TEMPLATE_S,
+    'Z': TEMPLATE_Z,
+    'L': TEMPLATE_L,
+    'J': TEMPLATE_J,
+    'T': TEMPLATE_T}
 
 
 def main():
@@ -25,16 +137,29 @@ def main():
 
 class TetrisGame:
     def __init__(self, screen, *board_size):
+        global width, height
+
+        # Иницилизация игровых переменных
+        self.level = None
+        self.fallFreq = None
+        self.width = width
+        self.height = height
+        self.score = 0
+        self.calculate_level_and_fall_frequency(self.score)
+
+        # Иницилизация счетчиков времени
+        self.lastMoveDownTime = time.time()
+        self.lastMoveSidewaysTime = time.time()
+        self.lastFallTime = time.time()
+
         self.surface = screen
 
         pygame.mixer.music.load('tetris.mp4')
 
-        lastMoveDownTime = time.time()
-        lastMoveSidewaysTime = time.time()
-        lastFallTime = time.time()
-
         self.intro_playing()
         self.menu_starting()
+
+        # Иницилизация игрового поля
         self.board = Board(*board_size, self.surface)
         self.board.generate_blank_board()
 
@@ -43,6 +168,19 @@ class TetrisGame:
 
     def menu_starting(self):
         pass
+
+    def calculate_level_and_fall_frequency(self, score):
+        self.level = int(score / 10) + 1
+        self.fallFreq = 0.27 - (self.level * 0.02)
+
+    def create_new_figure(self):
+        template = random.choice(list(TEMPLATES.keys()))
+        shape = {'shape': template,
+                 'rotation': random.randint(0, len(TEMPLATES[template]) - 1),
+                 'x': int(self.board.width / 2) - int(self.width / 2),
+                 'y': -2,
+                 'color': (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))}
+        return shape
 
 
 class Board:
